e95399201deb060b5448b924993077dd6d7d8f98
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 15:29:10 +0200

    Configured Application.kt by adding in modules

--- src/main/kotlin/rmc/Application.kt
+++ src/main/kotlin/rmc/Application.kt
@@ -3,16 +3,24 @@ package rmc
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import rmc.db.DatabaseFactory
+import rmc.error.configureStatusPages
 import rmc.plugins.*
+import javax.xml.crypto.Data
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
-    configureSerialization()
     configureSecurity()
-    configureDatabases()
+    configureSerialization()
     configureRouting()
+    configureStatusPages()
+    configureValidation()
+    DatabaseFactory.init()
 }
