0baa05f07e45627c99421fd3758f1bd3107511b5
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 12:27:19 +0200

    Initial commit

---
+++ src/main/kotlin/rmc/Application.kt
@@ -0,0 +1,18 @@
+package rmc
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import rmc.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    configureDatabases()
+    configureRouting()
+}
