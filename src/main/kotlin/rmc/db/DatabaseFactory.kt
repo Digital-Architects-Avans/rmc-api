efab50b89e680fcb95f8f2e0a4c508846bfad562
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 16:04:15 +0200

    Created DatabaseFactory - set up H2 db with Exposed Framework DAO API

---
+++ src/main/kotlin/rmc/db/DatabaseFactory.kt
@@ -0,0 +1,25 @@
+package rmc.db
+
+import kotlinx.coroutines.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.*
+import org.jetbrains.exposed.sql.transactions.experimental.*
+import rmc.model.UsersTable
+
+object DatabaseFactory {
+    fun init() {
+        val driverClassName = "org.h2.Driver"
+        val jdbcURL = "jdbc:h2:file:./build/db"
+        val database = Database.connect(jdbcURL, driverClassName)
+
+        transaction(database) {
+            addLogger(StdOutSqlLogger)
+
+            SchemaUtils.create(UsersTable)
+        }
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
+
