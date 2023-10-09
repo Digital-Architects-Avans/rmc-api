1c5f06c68a0228f9fee99a6f8b7c6b37bb38a3bf
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 16:23:25 +0200

    Created TokenManager for JWT Token generation and verification

---
+++ src/main/kotlin/rmc/utils/TokenManager.kt
@@ -0,0 +1,33 @@
+package rmc.utils
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import rmc.dto.user.UserDTO
+import io.ktor.server.config.*
+import java.util.*
+
+class TokenManager(config: HoconApplicationConfig) {
+    private val secret = config.property("jwt.secret").getString()
+    private val issuer = config.property("jwt.issuer").getString()
+    private val audience = config.property("jwt.audience").getString()
+    private val expirationDate = System.currentTimeMillis() + (60000 * 60) // 60 seconds * 60 = 1 hour
+
+    fun generateJWTToken(user: UserDTO): String {
+        return JWT.create()
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .withClaim("email", user.email)
+            .withClaim("userType", user.userType)
+            .withClaim("userId", user.id)
+            .withExpiresAt(Date(expirationDate))
+            .sign(Algorithm.HMAC256(secret))
+    }
+
+    fun verifyJWTToken(): JWTVerifier {
+        return JWT.require(Algorithm.HMAC256(secret))
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .build()
+    }
+}
