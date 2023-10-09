ae3ec475dcfcee0e2390e74da397355604c37795
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 16:18:21 +0200

    Created Validation.kt installed RequestValidation plugin

---
+++ src/main/kotlin/rmc/plugins/Validation.kt
@@ -0,0 +1,29 @@
+package rmc.plugins
+
+import rmc.dto.user.SignupDTO
+import io.ktor.server.application.*
+import io.ktor.server.plugins.requestvalidation.*
+import rmc.dto.user.SigninDTO
+
+fun Application.configureValidation() {
+    install(RequestValidation) {
+
+        validate<SignupDTO> { user ->
+            try {
+                user.validate()
+                ValidationResult.Valid
+            } catch (e: Exception) {
+                ValidationResult.Invalid(e.message!!)
+            }
+        }
+
+        validate<SigninDTO> { user ->
+            try {
+                user.validate()
+                ValidationResult.Valid
+            } catch (e: Exception) {
+                ValidationResult.Invalid(e.message!!)
+            }
+        }
+    }
+}
