032d9d907cf926dcb0a032ca6c6b95e51b7bab16
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 15:54:24 +0200

    Moved install of statusPages to Error.kt

--- src/main/kotlin/rmc/plugins/Routing.kt
+++ src/main/kotlin/rmc/plugins/Routing.kt
@@ -1,20 +1,14 @@
 package rmc.plugins
 
-import io.ktor.http.*
+import rmc.route.user.userRoutes
 import io.ktor.server.application.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import rmc.route.user.validateTokenRoute
 
 fun Application.configureRouting() {
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
-        }
-    }
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        userRoutes()
+        validateTokenRoute()
     }
 }
+
