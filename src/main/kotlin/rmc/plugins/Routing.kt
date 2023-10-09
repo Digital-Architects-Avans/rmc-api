52d1b0f5d80d8f5272faaddf470d49cf6fcbad02
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-09 16:39:46 +0200

    Removed old validateTokenRoute() module

--- src/main/kotlin/rmc/plugins/Routing.kt
+++ src/main/kotlin/rmc/plugins/Routing.kt
@@ -3,7 +3,6 @@ package rmc.plugins
 import rmc.route.user.userRoutes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import rmc.route.user.validateTokenRoute
 
 fun Application.configureRouting() {
     routing {
