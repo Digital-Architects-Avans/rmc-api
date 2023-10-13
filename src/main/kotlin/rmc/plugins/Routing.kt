8444bd7fc31d926e746eacd3c262696e30cedfe4
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:19:13 +0200

    Added vehicleRoutes and rentalRoutes

--- src/main/kotlin/rmc/plugins/Routing.kt
+++ src/main/kotlin/rmc/plugins/Routing.kt
@@ -3,10 +3,14 @@ package rmc.plugins
 import rmc.route.user.userRoutes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import rmc.route.rental.rentalRoutes
+import rmc.route.vehicle.vehicleRoutes
 
 fun Application.configureRouting() {
     routing {
         userRoutes()
+        vehicleRoutes()
+        rentalRoutes()
     }
 }
 
