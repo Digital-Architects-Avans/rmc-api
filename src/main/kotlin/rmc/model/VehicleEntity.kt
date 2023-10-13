9db7b086134a97a30846609a25acfb2ebc7fbba4
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:21:39 +0200

    created VehicleEntity

---
+++ src/main/kotlin/rmc/model/VehicleEntity.kt
@@ -0,0 +1,40 @@
+package rmc.model
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import rmc.db.VehiclesTable
+import rmc.dto.vehicle.VehicleDTO
+
+class VehicleEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<VehicleEntity>(VehiclesTable)
+
+    var brand by VehiclesTable.brand
+    var model by VehiclesTable.model
+    var year by VehiclesTable.year
+    var vehicleClass by VehiclesTable.vehicleClass
+    var engineType by VehiclesTable.engineType
+    var licensePlate by VehiclesTable.licensePlate
+    var imgLink by VehiclesTable.imgLink
+    var vehicleLocation by VehiclesTable.vehicleLocation
+    var price by VehiclesTable.price
+    var availability by VehiclesTable.availability
+
+    // Simple one to one reference from the UsersTable
+    var userId by VehiclesTable.userId
+}
+
+fun VehicleEntity.toVehicleDTO() = VehicleDTO(
+    this.id.value,
+    this.userId.value,
+    this.brand,
+    this.model,
+    this.year,
+    this.vehicleClass,
+    this.engineType,
+    this.licensePlate,
+    this.imgLink,
+    this.vehicleLocation,
+    this.price,
+    this.availability
+)
