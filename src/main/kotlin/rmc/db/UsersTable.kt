897fe5f89c443b9bf7613dcf8bcf63b9a30edfc4
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 15:21:13 +0200

    seperated and moved object UsersTable into db package

---
+++ src/main/kotlin/rmc/db/UsersTable.kt
@@ -0,0 +1,21 @@
+package rmc.db
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import rmc.dto.user.UserType
+
+object UsersTable : IntIdTable() {
+    val email = varchar("email", 100)
+    val userType = enumerationByName<UserType>("userType", 100)
+    val password = varchar("password", 100)
+    val firstName = varchar("firstName", 100).default("")
+    val lastName = varchar("lastName", 100).default("")
+    val phone = varchar("phone", 100).default("")
+    val street = varchar("street", 100).default("")
+    val buildingNumber = varchar("buildingNumber", 100).default("")
+    val zipCode = varchar("zipCode", 100).default("")
+    val city = varchar("city", 100).default("")
+
+    init {
+        index(true, email)
+    }
+}
