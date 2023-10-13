79a59e262304bc7bd1da99b717353ddde9bd4cac
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:16:54 +0200

    created CreateVehicleDTO

---
+++ src/main/kotlin/rmc/dto/vehicle/CreateVehicleDTO.kt
@@ -0,0 +1,16 @@
+package rmc.dto.vehicle
+
+import kotlinx.serialization.Serializable
+@Serializable
+data class CreateVehicleDTO (
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val vehicleClass: String,
+    val engineType: String,
+    val licensePlate: String,
+    val imgLink: String,
+    val vehicleLocation: String,
+    val price: Float,
+    val availability: String
+)
