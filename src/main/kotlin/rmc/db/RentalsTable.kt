ecaf4460f29f0cd5324b4ade72f10e9661a0ebb6
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:15:02 +0200

    created the RentalsTable.kt

---
+++ src/main/kotlin/rmc/db/RentalsTable.kt
@@ -0,0 +1,15 @@
+package rmc.db
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import rmc.model.RentalStatus
+
+object RentalsTable : IntIdTable() {
+    val vehicleId = reference("vehicleId", VehiclesTable.id)
+    val userId = reference("userId", UsersTable.id)
+    val date = varchar("date", 100)
+    val price = float("price").default(0.0F)
+    val location = varchar("location", 100).default("")
+    val status = enumerationByName<RentalStatus>("status", 100).default(RentalStatus.PENDING)
+    val distanceTravelled = float("distanceTravelled").default(0.0F)
+    val score = integer("score").default(100)
+}
