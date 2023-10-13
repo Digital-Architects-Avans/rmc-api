c4fa602583823e1e868f0f1a6d5ed8f7c7f815bf
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:14:39 +0200

    created the VehiclesTable.kt

---
+++ src/main/kotlin/rmc/db/VehiclesTable.kt
@@ -0,0 +1,21 @@
+package rmc.db
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object VehiclesTable : IntIdTable() {
+    val userId = reference("userId", UsersTable.id)
+    val brand = varchar("brand", 100)
+    val model = varchar("model", 100)
+    val year = integer("year").default(0)
+    val vehicleClass = varchar("vehicleClass", 100).default("")
+    val engineType = varchar("engineType", 100).default("")
+    val licensePlate = varchar("licensePlate", 100).default("")
+    val imgLink = varchar("imgLink", 100).default("")
+    val vehicleLocation = varchar("vehicleLocation", 100).default("")
+    val price = float("price").default(0.0F)
+    val availability = varchar("availability", 100).default("")
+
+    init {
+        index(true, licensePlate)
+    }
+}
