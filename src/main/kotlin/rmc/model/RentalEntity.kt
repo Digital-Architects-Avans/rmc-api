9d68fb9cf913db6ee8cc078787aedf81ffea5c12
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-13 22:18:30 +0200

    Created RentalEntity

---
+++ src/main/kotlin/rmc/model/RentalEntity.kt
@@ -0,0 +1,39 @@
+package rmc.model
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import rmc.db.RentalsTable
+import rmc.dto.rental.RentalDTO
+
+enum class RentalStatus(val status: String) {
+    PENDING("PENDING"),
+    APPROVED("APPROVED"),
+    DENIED("DENIED")
+}
+class RentalEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<RentalEntity>(RentalsTable)
+
+    var date by RentalsTable.date
+    var price by RentalsTable.price
+    var location by RentalsTable.location
+    var status by RentalsTable.status
+    var distanceTravelled by RentalsTable.distanceTravelled
+    var score by RentalsTable.score
+
+    // Simple one to one reference from the VehiclesTable and UsersTable
+    var userId by RentalsTable.userId
+    var vehicleId by RentalsTable.vehicleId
+}
+
+fun RentalEntity.toRentalDTO() = RentalDTO(
+    this.id.value,
+    this.vehicleId.value,
+    this.userId.value,
+    this.date,
+    this.price,
+    this.location,
+    this.status.status,
+    this.distanceTravelled,
+    this.score
+)
