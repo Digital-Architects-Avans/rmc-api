c829a26153253c0429562956621ade267bba57df
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-15 17:00:31 +0200

    switched function parameters and added UserId typealias

--- src/main/kotlin/rmc/dao/rental/RentalRepositoryImpl.kt
+++ src/main/kotlin/rmc/dao/rental/RentalRepositoryImpl.kt
@@ -4,16 +4,17 @@ import org.jetbrains.exposed.dao.id.EntityID
 import rmc.db.DatabaseFactory.dbQuery
 import rmc.db.UsersTable
 import rmc.db.VehiclesTable
+import rmc.db.entity.*
 import rmc.dto.rental.CreateRentalDTO
 import rmc.dto.rental.RentalDTO
+import rmc.dto.user.UserId
 import rmc.dto.vehicle.VehicleId
 import rmc.error.EntityWithIdNotFound
-import rmc.model.*
 
 class RentalRepositoryImpl : RentalRepository {
     override suspend fun createRental(
+        userId: UserId,
         vehicleId: VehicleId,
-        userId: Int,
         rental: CreateRentalDTO): RentalDTO = dbQuery {
             UserEntity.findById(userId) ?: throw EntityWithIdNotFound("User", userId)
             VehicleEntity.findById(vehicleId) ?: throw EntityWithIdNotFound("Vehicle", vehicleId)
