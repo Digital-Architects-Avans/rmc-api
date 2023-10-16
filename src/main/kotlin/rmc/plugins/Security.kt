8a47f5a04318ee3f93c271ce56ed71bbecef2a4c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-16 16:36:01 +0200

    Added CallLogging and RateLimit configurations

--- src/main/kotlin/rmc/plugins/Security.kt
+++ src/main/kotlin/rmc/plugins/Security.kt
@@ -8,9 +8,15 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.config.*
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.ratelimit.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.slf4j.event.Level
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSecurity() {
     val config = HoconApplicationConfig(ConfigFactory.load())
@@ -33,6 +39,24 @@ fun Application.configureSecurity() {
 
             }
 
+        }
+    }
+
+    install(CallLogging) {
+    level = Level.TRACE
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+        }
+    }
+
+    // Set a limit per request per requestKey, uses the client address as requestKey
+    install(RateLimit) {
+        register {
+            rateLimiter(limit = 10, refillPeriod = 60.seconds)
+            requestKey { call -> call.request.origin.remoteHost }
         }
     }
 }
