8607698206a37f92ed323f9668c9545a4b2cefdf
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-16 16:36:36 +0200

    Added rateLimit to UserRoutes

--- src/main/kotlin/rmc/route/user/UserRoutes.kt
+++ src/main/kotlin/rmc/route/user/UserRoutes.kt
@@ -12,6 +12,7 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.config.*
+import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -23,72 +24,73 @@ fun Route.userRoutes() {
     val userRepository = UserRepositoryImpl()
 
     route("/user") {
-        post("/signup") {
-            val signUpDTO = call.receive<SignupDTO>()
-
-            try {
-                val newUser = userRepository.createUser(signUpDTO)
-                val token = tokenManager.generateJWTToken(newUser)
-
-                call.response.header("Authorization", token)
-                call.respondText(Json.encodeToString(mapOf("token" to token)), status = HttpStatusCode.OK)
-            } catch (e: Exception) {
-                throw EmailAlreadyRegistered()
-            }
-        }
-
-        post("/signin") {
-            val signInDTO = call.receive<SigninDTO>()
-
-            try {
-                val authenticatedUser = userRepository.authenticateUser(signInDTO)
-                val token = tokenManager.generateJWTToken(authenticatedUser)
-
-                call.response.header("Authorization", token)
-                call.respondText(Json.encodeToString(mapOf("token" to token)), status = HttpStatusCode.OK)
-            } catch (e: Exception) {
-                throw AuthenticationFailed()
-            }
-        }
-
-        authenticate {
-            get("/users") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-
-                if (userType != "STAFF") throw WrongUserType()
-                val users = userRepository.allUsers()
-                call.respond(users)
-            }
-
-            get("/{id}") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-
-                if (userType != "STAFF") throw WrongUserType()
-                val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
-                val found = userRepository.getUserById(id)
-                found.let { call.respond(it) }
-            }
-
-            put("/{id}") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-
-                if (userType != "STAFF") throw WrongUserType()
-                val userId = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
-                val updateUserDTO = call.receive<UpdateUserDTO>()
+        rateLimit {
+            post("/signup") {
+                val signUpDTO = call.receive<SignupDTO>()
 
                 try {
-                    val existingUser = userRepository.getUserById(userId)
-                    if (existingUser.email != updateUserDTO.email) throw EmailUpdateAttemptError()
-                    userRepository.updateUser(userId, updateUserDTO)
-                    val updatedUser = userRepository.getUserById(userId)
-                    call.respond(updatedUser)
-                } catch (e: EntityWithIdNotFound) {
-                    val signUpDto = SignupDTO(updateUserDTO.email, updateUserDTO.userType, updateUserDTO.password)
-                    val newUser = userRepository.createUser(signUpDto)
-                    call.respond(newUser)
+                    val newUser = userRepository.createUser(signUpDTO)
+                    val token = tokenManager.generateJWTToken(newUser)
+
+                    call.response.header("Authorization", token)
+                    call.respondText(Json.encodeToString(mapOf("token" to token)), status = HttpStatusCode.OK)
+                } catch (e: Exception) {
+                    throw EmailAlreadyRegistered()
+                }
+            }
+            post("/signin") {
+                val signInDTO = call.receive<SigninDTO>()
+
+                try {
+                    val authenticatedUser = userRepository.authenticateUser(signInDTO)
+                    val token = tokenManager.generateJWTToken(authenticatedUser)
+
+                    call.response.header("Authorization", token)
+                    call.respondText(Json.encodeToString(mapOf("token" to token)), status = HttpStatusCode.OK)
+                } catch (e: Exception) {
+                    throw AuthenticationFailed()
+                }
+            }
+
+            authenticate {
+                get("/users") {
+                    val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
+                    val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
+
+                    if (userType != "STAFF") throw WrongUserType()
+                    val users = userRepository.allUsers()
+                    call.respond(users)
+                }
+
+                get("/{id}") {
+                    val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
+                    val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
+
+                    if (userType != "STAFF") throw WrongUserType()
+                    val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
+                    val found = userRepository.getUserById(id)
+                    found.let { call.respond(it) }
+                }
+
+                put("/{id}") {
+                    val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
+                    val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
+
+                    if (userType != "STAFF") throw WrongUserType()
+                    val userId = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
+                    val updateUserDTO = call.receive<UpdateUserDTO>()
+
+                    try {
+                        val existingUser = userRepository.getUserById(userId)
+                        if (existingUser.email != updateUserDTO.email) throw EmailUpdateAttemptError()
+                        userRepository.updateUser(userId, updateUserDTO)
+                        val updatedUser = userRepository.getUserById(userId)
+                        call.respond(updatedUser)
+                    } catch (e: EntityWithIdNotFound) {
+                        val signUpDto = SignupDTO(updateUserDTO.email, updateUserDTO.userType, updateUserDTO.password)
+                        val newUser = userRepository.createUser(signUpDto)
+                        call.respond(newUser)
+                    }
                 }
             }
 
@@ -100,6 +102,7 @@ fun Route.userRoutes() {
                 val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
                 call.respond(userRepository.deleteUser(id))
             }
+
 
             get("/me") {
                 val principal = call.principal<JWTPrincipal>()
