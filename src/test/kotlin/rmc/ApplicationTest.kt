0438278045061026f9087769184260f56a85aa0c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-16 18:17:33 +0200

    Created ApplicationTest.kt with first unitTest for /signup

--- src/test/kotlin/rmc/ApplicationTest.kt
+++ src/test/kotlin/rmc/ApplicationTest.kt
@@ -3,19 +3,63 @@ package rmc
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import rmc.db.dao.UserType
+import rmc.dto.user.SignupDTO
 import kotlin.test.*
-import rmc.plugins.*
+
+
+@Serializable
+data class Token(val token: String)
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
+    fun testPostUserSignup() = testApplication {
+        val jwtRegex = """^[a-zA-Z0-9]+?\.[a-zA-Z0-9]+?\..+""".toRegex()
+        val time = System.currentTimeMillis()
+        val userCredentials = SignupDTO("$time@staff.com", UserType.OTHER, "StrongPassword$time!")
+
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+
+        val response = client.post("/user/signup") {
+            contentType(ContentType.Application.Json)
+            setBody(userCredentials)
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        }
+
+        val principal = Json.decodeFromString<Token>(response.bodyAsText())
+
+        assert(principal.token.matches(jwtRegex))
+        assertEquals("""{"token":"${principal.token}"}""", response.bodyAsText())
+        assertEquals(HttpStatusCode.Created, response.status)
+    }
+
+    @Test
+    fun testPostVehicleCreatevehicle() = testApplication {
+        application {
+            module()
+        }
+        client.post("/vehicle/createVehicle").apply {
+            TODO("Please write your test here")
+        }
+    }
+
+    @Test
+    fun testPostRentalCreaterentalVehicleid() = testApplication {
+        application {
+            module()
+        }
+        client.post("/rental/createRental/{vehicleId}").apply {
+            TODO("Please write your test here")
         }
     }
 }
+
