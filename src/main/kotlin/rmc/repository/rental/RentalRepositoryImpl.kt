a966ef2762a5700c5c438a1025816c639b41ac70
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-17 17:42:04 +0200

    added CRUD functions to interface implementation for Rentals

--- src/main/kotlin/rmc/repository/rental/RentalRepositoryImpl.kt
+++ src/main/kotlin/rmc/repository/rental/RentalRepositoryImpl.kt
@@ -2,16 +2,29 @@ package rmc.repository.rental
 
 import org.jetbrains.exposed.dao.id.EntityID
 import rmc.db.DatabaseFactory.dbQuery
+import rmc.db.dao.*
 import rmc.db.tables.UsersTable
 import rmc.db.tables.VehiclesTable
-import rmc.db.dao.*
 import rmc.dto.rental.CreateRentalDTO
 import rmc.dto.rental.RentalDTO
+import rmc.dto.rental.RentalId
+import rmc.dto.rental.UpdateRentalDTO
 import rmc.dto.user.UserId
 import rmc.dto.vehicle.VehicleId
 import rmc.error.EntityWithIdNotFound
 
 class RentalRepositoryImpl : RentalRepository {
+
+    override suspend fun allRentals(): List<RentalDTO> = dbQuery {
+        RentalEntity.all().map {
+            it.toRentalDTO()
+        }
+    }
+
+    override suspend fun getRentalById(rentalId: RentalId): RentalDTO = dbQuery {
+        RentalEntity.findById(rentalId)?.toRentalDTO() ?: throw EntityWithIdNotFound("Rental", rentalId)
+    }
+
     override suspend fun createRental(
         userId: UserId,
         vehicleId: VehicleId,
@@ -29,5 +42,22 @@ class RentalRepositoryImpl : RentalRepository {
             distanceTravelled = rental.distanceTravelled
             score = rental.score
         }.toRentalDTO()
+    }
+
+    override suspend fun updateRental(rentalId: RentalId, rental: UpdateRentalDTO) = dbQuery {
+        RentalEntity.findById(rentalId)?.let {
+            it.vehicleId = EntityID(rental.vehicleId, VehiclesTable)
+            it.userId = EntityID(rental.userId, UsersTable)
+            it.date = rental.date
+            it.price = rental.price
+            it.location = rental.location
+            it.status = rental.status
+            it.distanceTravelled = rental.distanceTravelled
+            it.score = rental.score
+        } ?: throw EntityWithIdNotFound("Rental", rentalId)
+    }
+
+    override suspend fun deleteRental(rentalId: RentalId) = dbQuery {
+        UserEntity.findById(rentalId)?.delete() ?: throw EntityWithIdNotFound("Rental", rentalId)
     }
 }
