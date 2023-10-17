e22db63878f93f410a40d321b7747de861ce0f64
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-17 17:41:35 +0200

    added CRUS functions to interface for Rentals

--- src/main/kotlin/rmc/repository/rental/RentalRepository.kt
+++ src/main/kotlin/rmc/repository/rental/RentalRepository.kt
@@ -2,10 +2,16 @@ package rmc.repository.rental
 
 import rmc.dto.rental.CreateRentalDTO
 import rmc.dto.rental.RentalDTO
-import rmc.dto.user.UserId
+import rmc.dto.rental.RentalId
+import rmc.dto.rental.UpdateRentalDTO
+import rmc.dto.user.*
 import rmc.dto.vehicle.VehicleId
 
 interface RentalRepository {
+    suspend fun allRentals(): List<RentalDTO>
+    suspend fun getRentalById(rentalId: RentalId): RentalDTO
     suspend fun createRental(userId: UserId, vehicleId: VehicleId, rental: CreateRentalDTO): RentalDTO
+    suspend fun updateRental(rentalId: RentalId, rental: UpdateRentalDTO)
+    suspend fun deleteRental(rentalId: RentalId)
 
 }
