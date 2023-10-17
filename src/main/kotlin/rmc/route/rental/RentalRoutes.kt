fe87c0341af7d5788fda09ba41175694eb91a062
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-17 17:43:02 +0200

    created the rental routes, with new improved JWT authentication trough PipeLineContext function

--- src/main/kotlin/rmc/route/rental/RentalRoutes.kt
+++ src/main/kotlin/rmc/route/rental/RentalRoutes.kt
@@ -1,29 +1,62 @@
 package rmc.route.rental
 
-import rmc.error.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import rmc.repository.rental.RentalRepositoryImpl
+import rmc.db.dao.UserType
 import rmc.dto.rental.CreateRentalDTO
+import rmc.error.WrongIdFormatException
+import rmc.plugins.authorize
+import rmc.plugins.currentUserId
+import rmc.repository.rental.RentalRepositoryImpl
+import rmc.repository.user.userRepository
+
 
 fun Route.rentalRoutes() {
     val rentalRepository = RentalRepositoryImpl()
 
-    authenticate {
-        route("/rental") {
+    route("/rental") {
+        rateLimit {
+            get("/rentals") {
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.STAFF, user)
+
+                val rentals = rentalRepository.allRentals()
+                call.respond(rentals)
+            }
+
+            get("/{id}") {
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.STAFF, user)
+
+                val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
+                val found = rentalRepository.getRentalById(id)
+                found.let { call.respond(it) }
+            }
+
             post("/createRental/{vehicleId}") {
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.STAFF, user)
+
                 val createRentalDTO = call.receive<CreateRentalDTO>()
                 val vehicleId = call.parameters["vehicleId"]?.toInt() ?: throw WrongIdFormatException()
 
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userId = principal.payload.getClaim("userId")?.asInt() ?: throw AuthenticationFailed()
-
                 val vehicle = rentalRepository.createRental(userId, vehicleId, createRentalDTO)
                 call.respond(vehicle)
+            }
+
+            delete("/{id}") {
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.STAFF, user)
+
+                val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
+                call.respond(rentalRepository.deleteRental(id))
             }
         }
     }
