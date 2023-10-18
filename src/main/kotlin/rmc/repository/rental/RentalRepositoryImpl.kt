cb86491c0efa96afcd4d0fc097fc7e59800b3c23
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-18 16:33:40 +0200

    added getRentalByUserId, getRentalByVehicleId and updateRentalStatus

--- src/main/kotlin/rmc/repository/rental/RentalRepositoryImpl.kt
+++ src/main/kotlin/rmc/repository/rental/RentalRepositoryImpl.kt
@@ -3,6 +3,7 @@ package rmc.repository.rental
 import org.jetbrains.exposed.dao.id.EntityID
 import rmc.db.DatabaseFactory.dbQuery
 import rmc.db.dao.*
+import rmc.db.tables.RentalsTable
 import rmc.db.tables.UsersTable
 import rmc.db.tables.VehiclesTable
 import rmc.dto.rental.CreateRentalDTO
@@ -25,12 +26,25 @@ class RentalRepositoryImpl : RentalRepository {
         RentalEntity.findById(rentalId)?.toRentalDTO() ?: throw EntityWithIdNotFound("Rental", rentalId)
     }
 
+    override suspend fun getRentalByUserId(userId: UserId): List<RentalDTO> = dbQuery {
+        RentalEntity.find { RentalsTable.userId eq userId }.map { it.toRentalDTO() }.let {
+            it.ifEmpty { throw EntityWithIdNotFound("userId", userId) }
+        }
+    }
+
+    override suspend fun getRentalByVehicleId(vehicleId: VehicleId): List<RentalDTO> = dbQuery {
+        RentalEntity.find { RentalsTable.vehicleId eq vehicleId }.map { it.toRentalDTO() }.let {
+            it.ifEmpty { throw EntityWithIdNotFound("vehicleId", vehicleId) }
+        }
+    }
+
     override suspend fun createRental(
         userId: UserId,
         vehicleId: VehicleId,
-        rental: CreateRentalDTO): RentalDTO = dbQuery {
-            UserEntity.findById(userId) ?: throw EntityWithIdNotFound("User", userId)
-            VehicleEntity.findById(vehicleId) ?: throw EntityWithIdNotFound("Vehicle", vehicleId)
+        rental: CreateRentalDTO
+    ): RentalDTO = dbQuery {
+        UserEntity.findById(userId) ?: throw EntityWithIdNotFound("User", userId)
+        VehicleEntity.findById(vehicleId) ?: throw EntityWithIdNotFound("Vehicle", vehicleId)
 
         RentalEntity.new {
             this.vehicleId = EntityID(vehicleId, VehiclesTable)
@@ -54,6 +68,12 @@ class RentalRepositoryImpl : RentalRepository {
             it.status = rental.status
             it.distanceTravelled = rental.distanceTravelled
             it.score = rental.score
+        } ?: throw EntityWithIdNotFound("Rental", rentalId)
+    }
+
+    override suspend fun updateRentalStatus(rentalId: RentalId, status: RentalStatus) = dbQuery {
+        RentalEntity.findById(rentalId)?.let {
+            it.status = status
         } ?: throw EntityWithIdNotFound("Rental", rentalId)
     }
 
