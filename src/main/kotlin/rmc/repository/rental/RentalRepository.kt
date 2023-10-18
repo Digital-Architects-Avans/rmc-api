b8e97fd8bfd6704a815ce0301a7945bcc66e1ac0
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-18 16:33:06 +0200

    added func to interface

--- src/main/kotlin/rmc/repository/rental/RentalRepository.kt
+++ src/main/kotlin/rmc/repository/rental/RentalRepository.kt
@@ -1,17 +1,21 @@
 package rmc.repository.rental
 
+import rmc.db.dao.RentalStatus
 import rmc.dto.rental.CreateRentalDTO
 import rmc.dto.rental.RentalDTO
 import rmc.dto.rental.RentalId
 import rmc.dto.rental.UpdateRentalDTO
-import rmc.dto.user.*
+import rmc.dto.user.UserId
 import rmc.dto.vehicle.VehicleId
 
 interface RentalRepository {
     suspend fun allRentals(): List<RentalDTO>
     suspend fun getRentalById(rentalId: RentalId): RentalDTO
+    suspend fun getRentalByUserId(userId: UserId): List<RentalDTO>
+    suspend fun getRentalByVehicleId(vehicleId: VehicleId): List<RentalDTO>
     suspend fun createRental(userId: UserId, vehicleId: VehicleId, rental: CreateRentalDTO): RentalDTO
     suspend fun updateRental(rentalId: RentalId, rental: UpdateRentalDTO)
+    suspend fun updateRentalStatus(rentalId: RentalId, status: RentalStatus)
     suspend fun deleteRental(rentalId: RentalId)
 
 }
