05413cab5d5657e0b6f72a293ee8e1abd5d26549
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:15:52 +0200

    added custom Serializer for LocalDate and BigDecimal (latitude / longitude)

--- src/main/kotlin/rmc/plugins/Serialization.kt
+++ src/main/kotlin/rmc/plugins/Serialization.kt
@@ -3,9 +3,47 @@ package rmc.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
     }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = LocalDate::class)
+class LocalDateSerializer : KSerializer<LocalDate> {
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
+}
+
+object BigDecimalSerializer: KSerializer<BigDecimal> {
+    override fun deserialize(decoder: Decoder): BigDecimal {
+        return decoder.decodeDouble().toBigDecimal()
+    }
+
+    override fun serialize(encoder: Encoder, value: BigDecimal) {
+        encoder.encodeDouble(value.toDouble())
+    }
+
+    override val descriptor: SerialDescriptor
+        get() = PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
 }
