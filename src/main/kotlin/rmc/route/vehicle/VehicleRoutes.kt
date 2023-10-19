9040d6385fe2e4fd283a58ff8622e25ce0bcb612
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:19:20 +0200

    changed principal to more compact piece of code using currentUserId and authorize functions

--- src/main/kotlin/rmc/route/vehicle/VehicleRoutes.kt
+++ src/main/kotlin/rmc/route/vehicle/VehicleRoutes.kt
@@ -1,15 +1,19 @@
 package rmc.route.vehicle
 
-import rmc.error.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import rmc.dao.vehicle.VehicleRepositoryImpl
+import rmc.db.dao.UserType
 import rmc.dto.vehicle.CreateVehicleDTO
 import rmc.dto.vehicle.UpdateVehicleDTO
+import rmc.error.NotOwnerOfEntityWithId
+import rmc.error.WrongIdFormatException
+import rmc.plugins.authorize
+import rmc.plugins.currentUserId
+import rmc.repository.user.userRepository
+import rmc.repository.vehicle.VehicleRepositoryImpl
 
 fun Route.vehicleRoutes() {
     val vehicleRepository = VehicleRepositoryImpl()
@@ -17,65 +21,73 @@ fun Route.vehicleRoutes() {
     authenticate {
         route("/vehicle") {
             post("/createVehicle") {
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
                 val createVehicleDTO = call.receive<CreateVehicleDTO>()
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userId = principal.payload.getClaim("userId")?.asInt() ?: throw AuthenticationFailed()
+
                 val vehicle = vehicleRepository.createVehicle(userId, createVehicleDTO)
                 call.respond(vehicle)
             }
 
             get("/{id}") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-                if (userType != "STAFF" && userType != "CLIENT") throw WrongUserType()
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
                 val id = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
                 val found = vehicleRepository.getVehicleById(id)
                 found.let { call.respond(it) }
             }
 
             get("/all") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-                if (userType != "STAFF" && userType != "CLIENT") throw WrongUserType()
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
                 val vehicles = vehicleRepository.getAllVehicles()
                 call.respond(vehicles)
             }
 
             get("/user") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-                if (userType != "STAFF" && userType != "CLIENT") throw WrongUserType()
-                val id = principal.payload.getClaim("userId")?.asInt() ?: throw AuthenticationFailed()
-                val found = vehicleRepository.getVehiclesByUserId(id)
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
+                val found = vehicleRepository.getVehiclesByUserId(userId)
                 found.let { call.respond(it) }
             }
 
             put("/{id}") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-                if (userType != "STAFF" && userType != "CLIENT") throw WrongUserType()
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
                 val vehicleId = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
-                val userId = principal.payload.getClaim("userId")?.asInt() ?: throw AuthenticationFailed()
                 val updateVehicleDTO = call.receive<UpdateVehicleDTO>()
-                try {
-                    val checkVehicle = vehicleRepository.getVehicleById(vehicleId)
-                    if (checkVehicle.userId != userId) throw EntityWithIdNotFound(" oops ", vehicleId)
-                    vehicleRepository.updateVehicle(vehicleId, updateVehicleDTO)
-                    val updatedVehicle = vehicleRepository.getVehicleById(vehicleId)
-                    call.respond(updatedVehicle)
-                } catch (e: EntityWithIdNotFound) {
-                    call.respond("Vehicle $vehicleId for User $userId not found")
-                }
+
+                // Check if user is owner of vehicle
+                val vehicle = vehicleRepository.getVehicleById(vehicleId)
+                if (userId != vehicle.userId) throw NotOwnerOfEntityWithId("vehicle", userId)
+
+                vehicleRepository.updateVehicle(vehicleId, updateVehicleDTO)
+                val updatedVehicle = vehicleRepository.getVehicleById(vehicleId)
+                call.respond(updatedVehicle)
             }
 
             delete("/{id}") {
-                val principal = call.principal<JWTPrincipal>() ?: throw AuthenticationFailed()
-                val userType = principal.payload.getClaim("userType")?.asString() ?: throw AuthenticationFailed()
-                if (userType != "STAFF" && userType != "CLIENT") throw WrongUserType()
-                val userId = principal?.payload?.getClaim("userId")?.asInt() ?: throw AuthenticationFailed()
+                val userId = currentUserId()
+                val user = userRepository.getUserById(userId)
+                authorize(UserType.CLIENT, user)
+
+                // Check if user is owner of vehicle
                 val vehicleId = call.parameters["id"]?.toInt() ?: throw WrongIdFormatException()
-                val checkVehicle = vehicleRepository.getVehicleById(vehicleId)
-                if (checkVehicle.userId != userId) throw EntityWithIdNotFound("VehicleId $vehicleId for User", userId)
+                val vehicle = vehicleRepository.getVehicleById(vehicleId)
+                if (userId != vehicle.userId) throw NotOwnerOfEntityWithId("vehicle", userId)
+
+                // Cancel all rentals with this vehicleId
+
                 call.respond(vehicleRepository.deleteVehicle(vehicleId))
             }
         }
