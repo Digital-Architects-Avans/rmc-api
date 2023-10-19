e7ba6eb959758df7e7f016d6b229b82ded26aae8
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:19:56 +0200

    fixed typo in engineType enumeration collumn name and changed vehicleLocation to latitude / longitude

--- src/main/kotlin/rmc/db/tables/VehiclesTable.kt
+++ src/main/kotlin/rmc/db/tables/VehiclesTable.kt
@@ -1,7 +1,7 @@
 package rmc.db.tables
 
 import org.jetbrains.exposed.dao.id.IntIdTable
-import rmc.db.entity.EngineType
+import rmc.db.dao.EngineType
 
 object VehiclesTable : IntIdTable() {
     val userId = reference("userId", UsersTable.id)
@@ -9,10 +9,11 @@ object VehiclesTable : IntIdTable() {
     val model = varchar("model", 100)
     val year = integer("year").default(0)
     val vehicleClass =  varchar("vehicleClass", 100).default("")
-    val engineType = enumerationByName<EngineType>("EngineType", 100)
+    val engineType = enumerationByName<EngineType>("engineType", 100)
     val licensePlate = varchar("licensePlate", 100).default("")
     val imgLink = varchar("imgLink", 100).default("")
-    val vehicleLocation = varchar("vehicleLocation", 100).default("")
+    val latitude = decimal("latitude", 8, 6)
+    val longitude = decimal("longitude", 9, 6)
     val price = float("price").default(0.0F)
     val availability = bool("availability").default(true)
 
