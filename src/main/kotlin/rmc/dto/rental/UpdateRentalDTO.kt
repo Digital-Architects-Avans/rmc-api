3c03a74244fbdfc723314a0fc651db40cea2bdbe
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:16:05 +0200

    changed date serializable LocalDate and location to serializable latitude / longitude

--- src/main/kotlin/rmc/dto/rental/UpdateRentalDTO.kt
+++ src/main/kotlin/rmc/dto/rental/UpdateRentalDTO.kt
@@ -2,14 +2,22 @@ package rmc.dto.rental
 
 import kotlinx.serialization.Serializable
 import rmc.db.dao.RentalStatus
+import rmc.plugins.BigDecimalSerializer
+import rmc.plugins.LocalDateSerializer
+import java.math.BigDecimal
+import java.time.LocalDate
 
 @Serializable
 data class UpdateRentalDTO (
     val vehicleId: Int,
     val userId: Int,
-    val date: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val date: LocalDate,
     val price: Float,
-    val location: String,
+    @Serializable(with = BigDecimalSerializer::class)
+    val latitude: BigDecimal,
+    @Serializable(with = BigDecimalSerializer::class)
+    val longitude: BigDecimal,
     val status: RentalStatus,
     val distanceTravelled: Float,
     val score: Int
