7895b176cdd799434f5fbb0a54b59fc29001ad97
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:18:13 +0200

    moved VehicleRepository to repository package

--- src/main/kotlin/rmc/repository/vehicle/VehicleRepository.kt
+++ src/main/kotlin/rmc/repository/vehicle/VehicleRepository.kt
@@ -1,9 +1,16 @@
 package rmc.repository.vehicle
 
-import rmc.dto.vehicle.CreateVehicleDTO
-import rmc.dto.vehicle.VehicleDTO
 import rmc.dto.user.UserId
+import rmc.dto.vehicle.CreateVehicleDTO
+import rmc.dto.vehicle.UpdateVehicleDTO
+import rmc.dto.vehicle.VehicleDTO
+import rmc.dto.vehicle.VehicleId
 
 interface VehicleRepository {
     suspend fun createVehicle(userID: UserId, vehicle: CreateVehicleDTO): VehicleDTO
+    suspend fun getAllVehicles(): List<VehicleDTO>
+    suspend fun getVehicleById(vehicleId: VehicleId): VehicleDTO
+    suspend fun getVehiclesByUserId(userId: UserId): List<VehicleDTO>
+    suspend fun updateVehicle(vehicleId: VehicleId, vehicle: UpdateVehicleDTO)
+    suspend fun deleteVehicle(vehicleId: VehicleId)
 }
