0eb1bad319b7e453415d9aae131782ff6e2f2352
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 15:24:46 +0200

    set on cascade for userId and vehicleId reference

--- src/main/kotlin/rmc/db/tables/RentalsTable.kt
+++ src/main/kotlin/rmc/db/tables/RentalsTable.kt
@@ -1,12 +1,13 @@
 package rmc.db.tables
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.date
 import rmc.db.dao.RentalStatus
 
 object RentalsTable : IntIdTable() {
-    val vehicleId = reference("vehicleId", VehiclesTable.id)
-    val userId = reference("userId", UsersTable.id)
+    val vehicleId = integer("vehicleId").references(VehiclesTable.id, onDelete = ReferenceOption.CASCADE)
+    val userId = integer("userId").references(UsersTable.id, onDelete = ReferenceOption.CASCADE)
     val date = date("date")
     val price = float("price").default(0.0F)
     val latitude = decimal("latitude", 8, 6)
