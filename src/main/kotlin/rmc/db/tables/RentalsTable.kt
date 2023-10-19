564af9f80aa877c0767b61da1d60af8f8365d40d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-19 13:15:23 +0200

    changed date serializable LocalDate and location to serializable latitude / longitude

--- src/main/kotlin/rmc/db/tables/RentalsTable.kt
+++ src/main/kotlin/rmc/db/tables/RentalsTable.kt
@@ -1,14 +1,16 @@
 package rmc.db.tables
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.javatime.date
 import rmc.db.dao.RentalStatus
 
 object RentalsTable : IntIdTable() {
     val vehicleId = reference("vehicleId", VehiclesTable.id)
     val userId = reference("userId", UsersTable.id)
-    val date = varchar("date", 100)
+    val date = date("date")
     val price = float("price").default(0.0F)
-    val location = varchar("location", 100).default("")
+    val latitude = decimal("latitude", 8, 6)
+    val longitude = decimal("longitude", 9, 6)
     val status = enumerationByName<RentalStatus>("status", 100).default(RentalStatus.PENDING)
     val distanceTravelled = float("distanceTravelled").default(0.0F)
     val score = integer("score").default(100)
