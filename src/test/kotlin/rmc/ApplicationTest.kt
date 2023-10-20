04eedfb6d0612ff1990eb435402c9968caf7a87b
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2023-10-20 14:47:09 +0200

    3 unit tests voor signupDTO

--- src/test/kotlin/rmc/ApplicationTest.kt
+++ src/test/kotlin/rmc/ApplicationTest.kt
@@ -1,77 +1,64 @@
 package rmc
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.server.testing.*
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
+import org.junit.Test
 import rmc.db.dao.UserType
 import rmc.dto.user.SignupDTO
-import rmc.dto.vehicle.CreateVehicleDTO
-import kotlin.test.*
+import rmc.error.WrongFormat
+import kotlin.test.fail
 
 
-@Serializable
-data class Token(val token: String)
 
-class ApplicationTest {
+class SignupDTOTest {
     @Test
-    fun testPostUserSignup() = testApplication {
-        val jwtRegex = """^[a-zA-Z0-9]+?\.[a-zA-Z0-9]+?\..+""".toRegex()
-        val time = System.currentTimeMillis()
-        val userCredentials = SignupDTO("$time@staff.com", UserType.OTHER, "StrongPassword$time!")
+    fun testValidInput() {
 
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
+        val validSignupDTO = SignupDTO(
+            email = "test@example.com",
+            userType = UserType.CLIENT,
+            password = "Password1#"
+        )
+
+        try {
+            validSignupDTO.validate()
+            println("Test 'testValidInput' passed successfully!")
+        } catch (e: Exception) {
+            fail("Test 'testValidInput' failed: ${e.message}")
         }
-
-        val response = client.post("/user/signup") {
-            contentType(ContentType.Application.Json)
-            setBody(userCredentials)
-            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-        }
-
-        val principal = Json.decodeFromString<Token>(response.bodyAsText())
-
-        assert(principal.token.matches(jwtRegex))
-        assertEquals("""{"token":"${principal.token}"}""", response.bodyAsText())
-        assertEquals(HttpStatusCode.Created, response.status)
     }
 
     @Test
-    fun testPostVehicleCreatevehicle() = testApplication {
-        val vehicle = CreateVehicleDTO()
+    fun testInvalidEmail() {
 
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
+        val invalidSignupDTO = SignupDTO(
+            email = "invalid_email",
+            userType = UserType.CLIENT,
+            password = "Password1#"
+        )
+
+        try {
+            invalidSignupDTO.validate()
+            fail("Test 'testInvalidEmail' failed: No exception thrown")
+        } catch (e: WrongFormat) {
+            println("Test 'testInvalidEmail' passed successfully!")
         }
-
-        val response = client.post("/vehicle/createVehicle") {
-            contentType(ContentType.Application.Json)
-            setBody(vehicle)
-            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-        }
-
-//        assert(principal.token.matches(jwtRegex))
-//        assertEquals("""{"token":"${principal.token}"}""", response.bodyAsText())
-//        assertEquals(HttpStatusCode.Created, response.status)
     }
 
     @Test
-    fun testPostRentalCreaterentalVehicleid() = testApplication {
-        application {
-            module()
-        }
-        client.post("/rental/createRental/{vehicleId}").apply {
-            TODO("Please write your test here")
+    fun testInvalidPassword() {
+
+        val invalidSignupDTO = SignupDTO(
+            email = "test@example.com",
+            userType = UserType.CLIENT,
+            password = "weak"
+        )
+
+        try {
+            invalidSignupDTO.validate()
+            fail("Test 'testInvalidPassword' failed: No exception thrown")
+        } catch (e: WrongFormat) {
+            println("Test 'testInvalidPassword' passed successfully!")
         }
     }
+
 }
 
